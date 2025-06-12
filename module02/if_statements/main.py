c97bcf90cc29fde4c74d6acd28e1d4944924f9f8
Oleh Andrus <andrusoleg123@gmail.com>
2025-06-12 22:02:22 +0300

    module03/04

--- module02/if_statements/main.py
+++ module02/if_statements/main.py
@@ -1,23 +1,5 @@
-age = int(input("What is your age >>> "))
-
-age = 23
-
-type(age) is int
-
-isinstance(age, int)
-
-# 4 spaces or 1 tab
-if age == 17:
-    print("You almost can drink alcohol!")
+name = input("What is your name >>> ")
 
 
-if age == 18:
-    print("You are old now!")
-elif age == 18:
-    print("You are old now!")
-elif age == 19:
-    print("You are old now!")
-elif age == 20:
-    print("You are old now!")
-else:
-    print("Go to school my friend!")
+if name.lower() == "oleh":
+    print("Welcome")
